6048ba13ad7b9b4970d0c34fe8fcfc13ad7adac1
w-bonelli <wpb3hw@virginia.edu>
2022-09-08 21:34:32 -0400

    refactor: merge test scripts

---
+++ scripts/test/test.ps1
@@ -0,0 +1,31 @@
+$path=$args[0]
+if (!($path)) {
+    write-output "Must specify path argument"
+    exit 1
+}
+
+if (test-path $path)
+{
+    write-output "Found install location: $path"
+} else {
+    write-output "Install location doesn't exist: $path"
+    exit 1
+}
+
+if ((get-command "ifort" -ErrorAction SilentlyContinue) -eq $null) {
+    write-output "Command ifort not available"
+    exit 1
+} else {
+    write-output "Command ifort found"
+}
+
+ifort scripts/test/hw.f90 -o hw
+write-output "Compile succeeded"
+
+$output=$(./hw)
+if ($output -match "hello world") {
+    write-output $output
+} else {
+    write-output "Unexpected output: $output"
+    exit 1
+}
--- scripts/test/test_compile.ps1
+++
@@ -1,8 +0,0 @@
-cmd.exe /c 'scripts/install/build_windows.bat'
-ifort scripts/test/hw.f90 -o hw
-$output=$(./hw)
-if ($output -contains "hello world") {
-    echo "Compiled and ran successfully, output: $output"
-} else {
-    exit 1
-}
