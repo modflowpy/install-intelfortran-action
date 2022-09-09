fe24689ae9082cc7bd219d22a38253ca3459da19
w-bonelli <wesbonelli@gmail.com>
2022-09-09 09:05:51 -0400

    Initial implementation (#1)
    * Initial implementation
    * ci: test compile hello world
    * refactor: merge test scripts

---
+++ scripts/test/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+path="$1"
+if [ -z "$path" ]
+then
+  echo "Must specify path argument"
+  exit 1
+fi
+
+if [ -d "$path" ]
+then
+  echo "Install location exists: $path"
+else
+  echo "Install location doesn't exist: $path"
+  exit 1
+fi
+
+if command -v ifort &> /dev/null
+then
+  echo "Command ifort available"
+else
+  echo "Command ifort not available"
+  exit 1
+fi
+
+ifort scripts/test/hw.f90 -o hw
+echo "Compile succeeded"
+
+output=$(./hw '2>&1')
+if [[ "$output" == *"hello world"* ]]
+then
+  echo "$output"
+else
+  echo "Unexpected output: $output"
+  exit 1
+fi
